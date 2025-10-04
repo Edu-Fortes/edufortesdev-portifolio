76700583a9b8878f8750894fb58482f1d6e31a64
Edu-Fortes <edu.mfortes@gmail.com>
2025-10-04 14:38:38 -0300

    feat(data): create interface and array of data to populate list in Navmenu component

---
+++ src/data/navmenuItemListData.ts
@@ -0,0 +1,24 @@
+import { INavmenuItemList } from '@/interface/INavmenuItemList';
+
+export const navmenuItemListData: INavmenuItemList[] = [
+  {
+    id: 1,
+    title: 'About',
+    href: '#',
+  },
+  {
+    id: 2,
+    title: 'Projects',
+    href: '#',
+  },
+  {
+    id: 3,
+    title: 'Workflow',
+    href: '#',
+  },
+  {
+    id: 4,
+    title: 'Contact',
+    href: '#',
+  },
+];
