01cd7f8b82bc4c5b93bffe361336c87d9ced10bc
Edu-Fortes <edu.mfortes@gmail.com>
2025-10-05 16:14:41 -0300

    feat(Features): add Features component and integrate into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
+import Features from '@/components/Features';
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar/Navbar';
 import { ThemeToggle } from '@/components/ThemeToggle';
 
 export default function Home() {
   return (
-    <div className="font-sans">
+    <div className="space-y-15 font-sans">
       <header>
         <Navbar />
         <Hero />
-        <ThemeToggle />
       </header>
+      <main>
+        <Features />
+      </main>
+      <ThemeToggle />
     </div>
   );
 }
