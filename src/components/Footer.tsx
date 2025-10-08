ac4f01c7785dce1e78100e4a91d3efe34b8cc088
Edu-Fortes <edu.mfortes@gmail.com>
2025-10-08 11:34:06 -0300

    feat(social-media): refactor SocialMediaIcons to use separate icon components for GitHub, Instagram, and WhatsApp; update Footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer({ children }: { children: React.ReactNode }) {
   return (
-    <footer className="px-4">
+    <footer className="mb-6 px-4">
       <h2 className="mb-8 text-center text-3xl font-bold">
         Ready to take <span className="text-ring">your</span> digital presence
         to the next level?
