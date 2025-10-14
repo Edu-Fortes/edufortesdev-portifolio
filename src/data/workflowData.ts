02ed95e002cac74779b1944ee99fbebeaba889e1
Edu-Fortes <edu.mfortes@gmail.com>
2025-10-13 22:11:23 -0300

    feat: update Workflow component and workflowData with Portuguese translations

--- src/data/workflowData.ts
+++ src/data/workflowData.ts
@@ -4,25 +4,28 @@ export const workflowData: ICardWorkflowProps[] = [
   {
     data: {
       id: 1,
-      phase: 'Phase 1',
-      title: 'Planning',
-      description: 'Define project scope, objectives, and deliverables.',
+      phase: 'Fase 1',
+      title: 'Planejamento',
+      description:
+        'Tudo começa com um mergulho profundo na sua visão. Definimos seus objetivos, entendemos seu público e traçamos um plano claro e estratégico para o projeto.',
     },
   },
   {
     data: {
       id: 2,
-      phase: 'Phase 2',
-      title: 'Design',
-      description: 'Create wireframes and prototypes for the project.',
+      phase: 'Fase 2',
+      title: 'Desenvolvimento e Progresso',
+      description:
+        'Eu construo seu site minuciosamente, do zero, e mantenho você envolvido com atualizações constantes e oportunidades de feedback. Você verá suas ideias ganharem vida, pixel por pixel.',
     },
   },
   {
     data: {
       id: 3,
-      phase: 'Phase 3',
-      title: 'Development',
-      description: 'Implement the project based on the approved designs.',
+      phase: 'Fase 3',
+      title: 'Revisão e Lançamento',
+      description:
+        'Testamos cada detalhe rigorosamente, garantindo que seu site esteja livre de bugs, totalmente responsivo e otimizado para a performance. Quando estiver pronto, lançamos um produto digital feito para o sucesso.',
     },
   },
 ];
